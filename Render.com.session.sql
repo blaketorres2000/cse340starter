e3aa13e8f5273638b976a5c63b62544d9d575329
Use your GitHub username <Enter your email address>
2023-10-12 08:26:32 -0500

    something strange happened

--- Render.com.session.sql
+++
@@ -1,1 +0,0 @@
-SELECT inventory.inv_make, inventory.inv_model, classification.classification_name FROM inventory INNER JOIN classification ON inventory.classification_id = classification.classification_id WHERE classification.classification_name = 'Sport';
