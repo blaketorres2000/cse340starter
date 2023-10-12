85040c5bf5617e5d04f9a56d330f0e02508c0d0e
Use your GitHub username <Enter your email address>
2023-10-12 08:20:14 -0500

    catch block added to baseController

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,9 +1,27 @@
-const utilities = require("../utilities/")
-const baseController = {}
+// const utilities = require("../utilities/")
+// const baseController = {}
 
-baseController.buildHome = async function(req, res){
-  const nav = await utilities.getNav()
-  res.render("index", {title: "Home", nav})
-}
+// baseController.buildHome = async function(req, res){
+//   const nav = await utilities.getNav()
+//   res.render("index", {title: "Home", nav})
+// }
 
-module.exports = baseController
+// module.exports = baseController
+
+const utilities = require("../utilities/");
+
+const baseController = {};
+
+baseController.buildHome = async function (req, res, next) {
+  try {
+    const nav = await utilities.getNav();
+    res.render("index", { title: "Home", nav });
+  } catch (error) {
+    // Handle the error here, for example, by logging it or rendering an error page.
+    console.error("An error occurred in baseController.buildHome:", error);
+    // You can also pass the error to an error handling middleware.
+    next(error);
+  }
+};
+
+module.exports = baseController;
