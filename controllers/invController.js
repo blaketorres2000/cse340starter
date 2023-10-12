a43247ec4f76a150ef91ce353be7498705dbce2e
Use your GitHub username <Enter your email address>
2023-10-12 08:18:58 -0500

    catch block added to invController

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,22 +1,52 @@
-const invModel = require("../models/inventory-model")
-const utilities = require("../utilities/")
+// const invModel = require("../models/inventory-model")
+// const utilities = require("../utilities/")
 
-const invCont = {}
+// const invCont = {}
+
+// /* ***************************
+//  *  Build inventory by classification view
+//  * ************************** */
+// invCont.buildByClassificationId = async function (req, res, next) {
+//   const classification_id = req.params.classificationId
+//   const data = await invModel.getInventoryByClassificationId(classification_id)
+//   const grid = await utilities.buildClassificationGrid(data)
+//   let nav = await utilities.getNav()
+//   const className = data[0].classification_name
+//   res.render("./inventory/classification", {
+//     title: className + " vehicles",
+//     nav,
+//     grid,
+//   })
+// }
+
+// module.exports = invCont
+
+const invModel = require("../models/inventory-model");
+const utilities = require("../utilities/");
+
+const invCont = {};
 
 /* ***************************
  *  Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-  const classification_id = req.params.classificationId
-  const data = await invModel.getInventoryByClassificationId(classification_id)
-  const grid = await utilities.buildClassificationGrid(data)
-  let nav = await utilities.getNav()
-  const className = data[0].classification_name
-  res.render("./inventory/classification", {
-    title: className + " vehicles",
-    nav,
-    grid,
-  })
-}
+  try {
+    const classification_id = req.params.classificationId;
+    const data = await invModel.getInventoryByClassificationId(classification_id);
+    const grid = await utilities.buildClassificationGrid(data);
+    let nav = await utilities.getNav();
+    const className = data[0].classification_name;
+    res.render("./inventory/classification", {
+      title: className + " vehicles",
+      nav,
+      grid,
+    });
+  } catch (error) {
+    // Handle the error here, for example, by logging it or rendering an error page.
+    console.error("An error occurred in invCont.buildByClassificationId:", error);
+    // You can also pass the error to an error handling middleware.
+    next(error);
+  }
+};
 
-module.exports = invCont
+module.exports = invCont;
